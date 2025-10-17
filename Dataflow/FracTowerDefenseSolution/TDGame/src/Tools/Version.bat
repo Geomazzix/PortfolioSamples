231120ce9170e3f3f5b9c8cf482aa66776d0e724
Tom Lemmers <gjhlemmers@gmail.com>
2025-10-17 16:48:02 +0200

    Update Version.bat

--- Dataflow/FracTowerDefenseSolution/TDGame/src/Tools/Version.bat
+++ Dataflow/FracTowerDefenseSolution/TDGame/src/Tools/Version.bat
@@ -3,8 +3,8 @@ p4 changes -s submitted -m 1 //Projects2/Y2020D-Y2-DataFlow/FracTowerDefenseSolu
 if %ERRORLEVEL% NEQ 0 (
 	ECHO Change 000000 >>Version.txt
 ) else (
-	p4 changes -s submitted -m 1 //Projects2/Y2020D-Y2-DataFlow/FracTowerDefenseSolution/... >>Version.txt
-	)
+<omitted>
+)
 ECHO ) >>Version.txt
 
 exit /b 0
